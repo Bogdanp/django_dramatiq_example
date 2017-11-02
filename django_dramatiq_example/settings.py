2dc98bc13e37b690a0392e03a0b3fe100dd8460b
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-02 10:57:29 +0200

    chore: set conn max age

--- django_dramatiq_example/settings.py
+++ django_dramatiq_example/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'].update(dj_database_url.config())
+DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 # Tasks
 
