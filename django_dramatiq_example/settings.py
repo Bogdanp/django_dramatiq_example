9914ce545351c6633cea56ead1acf70d5b598e91
Bogdan Popa <popa.bogdanp@gmail.com>
2017-11-02 11:03:17 +0200

    chore: add staticfiles config

--- django_dramatiq_example/settings.py
+++ django_dramatiq_example/settings.py
@@ -140,4 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
