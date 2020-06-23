e8d372b4f651f3ece2bb3c4f442145eac80c58c2
Manny E <manny@3wcorner.com>
2020-06-23 05:23:06 -0400

    A bit of cleanup and we are good. Please note, for production, do not use manage.py runserver. use gunicorn instead.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#MYSQL_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
-#DOCKER_HOST=$(ip route show default | awk '/default/ {print $3}')
-#DOCKER_HOST=$(/sbin/ip route | awk '/default/ { print $3 }')
-#echo "DOCKER HOST: ${DOCKER_HOST}"
-#export MYSQL_HOST=$(echo $DOCKER_HOST)
-#echo "MYSQL HOST: ${MYSQL_HOST}"
+# This is to make sure it only runs the first time the container is starting.
+# no need to do migration if the code did not change.
 FILE=/code/installed.txt
 if [[ -f "$FILE" ]]; then
     echo "$FILE exists. starting server"
@@ -18,26 +14,9 @@ else
 	touch $FILE
 fi
 
-#echo "Starting celery services.."
-#echo "starting celery workers"
-
-#celery -A app worker -S django -l info -c 2 --pool=gevent &
-# celery -A app worker -S django -l debug --pool=solo
-#echo "starting celery task scheduler"
-#celery -A app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-# celery -A app beat -l debug --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-
-#echo "Starting Dev Mail Server..."
-#python -m smtpd -n -c DebuggingServer localhost:1025 &
-
-#BACKEND_HOST="${BACKEND_HOST:-0.0.0.0}"
-#BACKEND_PORT="${BACKEND_PORT:-8000}"
-
-#echo "Starting dvelopment Server"
-#python manage.py runserver 0.0.0.0:${BACKEND_PORT}
-#gunicorn --bind $BACKEND_HOST:$BACKEND_PORT app.wsgi
-
+echo "starting workers workers"
 python manage.py rundramatiq -p4 -t4 &
 
+echo "Starting app with gunicorn...."
 gunicorn -c /code/gunicorn_config.py django_dramatiq_example.wsgi
 
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,5 @@
 import os
+# you can set ENV VARS when you run the image...
 backend_host=os.environ.get('BACKEND_HOST', "0.0.0.0")
 backend_port=os.environ.get('BACKEND_PORT', "8888")
 
